cf3567525c05eafa5857550e8317068e2b6f3b97
Kaappo Raivio <kaappo.raivio@gmail.com>
2021-09-14 01:26:24 +0300

    holy shit c++ is fast

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 
 int main () {
 
-    Camera camera = {{0, -5, 7}, {0, 0.25}, 1, {1, 1,}, {4000, 4000}};
+    Camera camera = {{0, -5, 7}, {0, 0.25}, 1, {1, 1,}, {2000, 2000}};
 
 //    auto il = {Vector3{-5, 6, 5}, {0,  0, 3}, {5,  6, 3}};
 //    std::unique_ptr<Triangle> triangle = std::make_unique<Triangle>(il);
@@ -29,13 +29,14 @@ int main () {
 
     std::vector<SceneObject> objects = {
             SceneObject{&t, m1},
-            SceneObject(&p, m2)
+            SceneObject{&p, m2},
     };
 //    std::vector<SceneObject> objects = {SceneObject{&t, m}};
     std::vector<LightSource> lights = {
 //            LightSource{{0, 0, 7}, Intensity{1, 0.5, 1} * 25},
-//            LightSource{{0, 5, 15}, Intensity{1, 1, 1} * 1},
-            LightSource{{-2, 3, 10}, Intensity{1, 1, 0} * 20}
+            LightSource{{0, 5, 5}, Intensity{1, 0, 0.25} * 5},
+            LightSource{{-2, 3, 10}, Intensity{1, 1, 0} * 1},
+            LightSource{{-2, 3, 10}, Intensity{1, 1, 0} * 1},
     };
 
     Scene scene{objects, lights, camera};
@@ -62,7 +63,7 @@ int main () {
 
 
 
-    MyOpenGLWindow window = {4000, 4000, 2, 1};
+    MyOpenGLWindow window = {2000, 2000, 2, 1};
     window.paint(pixels);
     window.delay(50000);
 //    while (true);
